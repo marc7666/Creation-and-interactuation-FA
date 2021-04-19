d4d4649215e4fb113d529a3c63ec813cdf861696
marc7666 <64266158+marc7666@users.noreply.github.com>
2021-04-19 14:16:48 +0200

    Lectura de dades ja correcta sol falta posar-ho bonic

--- src/LecturaAutomat.java
+++ src/LecturaAutomat.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 
 public class LecturaAutomat extends CommandLineProgram {
     public void run() {
-        int nombreEstats = readInt("Introdueix el nombre d'estats de l'autòmat: ");
-        int estatsFinalsNombre = readInt("Quants estats finals té l'autòmat? ");
+        int nombreEstats = readInt("Introdueix el nombre d'estats de l'autòmat: \n");
+        int estatsFinalsNombre = readInt("Quants estats finals té l'autòmat? \n");
         int[] estatsFinals = introduccioestatsFinals(estatsFinalsNombre);
-        int alfabetNombre = readInt("Quants símbols té l'alfabet? ");
-        boolean esBinari = readBoolean("És una alfabet binari? ");
+        int alfabetNombre = readInt("Quants símbols té l'alfabet? \n");
+        boolean esBinari = readBoolean("És una alfabet binari? \n");
         int[] alfabet1 = new int[0];
         String[] alfabet2 = new String[0];
         if (esBinari) {
@@ -16,9 +16,14 @@ public class LecturaAutomat extends CommandLineProgram {
         } else {
             alfabet2 = introduccioAlfabetLletres(alfabetNombre);
         }
+        veureDades(nombreEstats, estatsFinalsNombre, estatsFinals, alfabetNombre, esBinari, alfabet1, alfabet2);
+    }
+
+    private void veureDades(int nombreEstats, int estatsFinalsNombre, int[] estatsFinals, int alfabetNombre, boolean esBinari, int[] alfabet1, String[] alfabet2) {
+        print("NOTA: els estats es numeren de la següent manera: 1, 2, 3, ..., n \n");
         print("Nombre d'estats: " + nombreEstats + "\n");
         print("Nombre d'estats finals: " + estatsFinalsNombre + "\n");
-        System.out.println("Estats finals: " + Arrays.toString(estatsFinals) + "\n");
+        print("Estats finals: " + Arrays.toString(estatsFinals) + "\n");
         print("Nombre de simbols de l'alfabet: " + alfabetNombre + "\n");
         print("Binari?" + esBinari + "\n");
         if (esBinari) {
@@ -31,7 +36,7 @@ public class LecturaAutomat extends CommandLineProgram {
     private String[] introduccioAlfabetLletres(int alfabetNombre) {
         String[] alfabet = new String[alfabetNombre];
         for (int i = 0; i < alfabetNombre; i++) {
-            alfabet[i] = readLine("Introdueix el símbol " + (i + 1) + ": ");
+            alfabet[i] = readLine("Introdueix el símbol " + (i + 1) + ": \n");
         }
         return alfabet;
     }
@@ -39,7 +44,7 @@ public class LecturaAutomat extends CommandLineProgram {
     private int[] introduccioAlfabetBinari(int alfabetNombre) {
         int[] alfabet = new int[alfabetNombre];
         for (int i = 0; i < alfabetNombre; i++) {
-            alfabet[i] = readInt("Introdueix el símbol " + (i + 1) + ": ");
+            alfabet[i] = readInt("Introdueix el símbol " + (i + 1) + ": \n");
         }
         return alfabet;
     }
@@ -47,7 +52,7 @@ public class LecturaAutomat extends CommandLineProgram {
     private int[] introduccioestatsFinals(int nombreFinals) {
         int[] finals = new int[nombreFinals];
         for (int i = 0; i < nombreFinals; i++) {
-            finals[i] = readInt("Introdueix l'estat final " + (i + 1) + " ");
+            finals[i] = readInt("Introdueix l'estat final " + (i + 1) + " \n");
         }
         return finals;
     }
