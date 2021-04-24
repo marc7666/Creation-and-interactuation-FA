0342b605a18659674e098c54e449e8401b729f65
marc7666 <64266158+marc7666@users.noreply.github.com>
2021-04-24 18:10:43 +0200

    Lectura de dades ja correcta i acabada

--- src/LecturaAutomat.java
+++ src/LecturaAutomat.java
@@ -3,39 +3,43 @@ import acm.program.CommandLineProgram;
 import java.util.Arrays;
 
 public class LecturaAutomat extends CommandLineProgram {
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+
     public void run() {
-        int nombreEstats = readInt("Introdueix el nombre d'estats de l'autòmat: \n");
-        int estatsFinalsNombre = readInt("Quants estats finals té l'autòmat? \n");
-        int[] estatsFinals = introduccioestatsFinals(estatsFinalsNombre);
-        boolean esBinari = readBoolean("És una alfabet binari? (True o False) \n");
-        int[] alfabet1 = new int[0];
-        String[] alfabet2 = new String[0];
-        int alfabetNombre = 0;
-        if (esBinari) {
-            alfabet1 = new int[]{0, 1};
-            alfabetNombre = 2;
-        } else {
-            alfabetNombre = readInt("Quants símbols té l'alfabet? \n");
-            alfabet2 = introduccioAlfabetLletres(alfabetNombre);
-        }
-        veureDades(nombreEstats, estatsFinalsNombre, estatsFinals, alfabetNombre, esBinari, alfabet1, alfabet2);
+        /*Lectura de dades de l'autòmat*/
+        System.out.println(ANSI_CYAN + "---------- Lectura de dades ----------");
+        int nombreEstats = readInt(ANSI_RESET + "Introdueix el nombre d'estats de l'autòmat: \n"); //Introduccio nombre estats autòmat
+        int estatsFinalsNombre = readInt("Quants estats finals té l'autòmat? \n"); //Introduccio nombre estats finals
+        String[] estatsFinals = introduccioestatsFinals(estatsFinalsNombre); //Introduccio estats finals
+        int alfabetNombre = readInt("Quants símbols té l'alfabet? \n"); //Introduccio nombre símbols alfabet
+        String[] alfabet = introduccioAlfabet(alfabetNombre); //Introduccio alfabet
+        /*--------------------------------------------------------------------------------------------------------------------*/
+        /*Visualitzacio de dades per pantalla*/
+        System.out.println(ANSI_CYAN + "---------- Visualitzacio de dades ----------" + ANSI_RESET);
+        veureDades(nombreEstats, estatsFinalsNombre, estatsFinals, alfabetNombre, alfabet); //Visualitzacio de dades per pantalla
     }
 
-    private void veureDades(int nombreEstats, int estatsFinalsNombre, int[] estatsFinals, int alfabetNombre, boolean esBinari, int[] alfabet1, String[] alfabet2) {
-        print("NOTA: els estats es numeren de la següent manera: 1, 2, 3, ..., n \n");
-        print("Nombre d'estats: " + nombreEstats + "\n");
-        print("Nombre d'estats finals: " + estatsFinalsNombre + "\n");
-        print("Estats finals: " + Arrays.toString(estatsFinals) + "\n");
-        print("Nombre de simbols de l'alfabet: " + alfabetNombre + "\n");
-        print("Binari? " + esBinari + "\n");
-        if (esBinari) {
-            print("Alfabet: " + Arrays.toString(alfabet1) + "\n");
-        } else {
-            print("Alfabet: " + Arrays.toString(alfabet2) + "\n");
-        }
+
+    /**
+     * Aquesta funcio treu per pantalla totes les dades per pantalla
+     **/
+    private void veureDades(int nombreEstats, int estatsFinalsNombre, String[] estatsFinals, int alfabetNombre, String[] alfabet) {
+        System.out.println(ANSI_YELLOW + "NOTA: els estats es numeren de la següent manera: 1, 2, 3, ..., n");
+        System.out.println(ANSI_PURPLE + "Nombre d'estats: " + ANSI_RESET + nombreEstats);
+        System.out.println(ANSI_PURPLE + "Nombre d'estats finals: " + ANSI_RESET + estatsFinalsNombre);
+        System.out.println(ANSI_PURPLE + "Estats finals: " + ANSI_RESET + Arrays.toString(estatsFinals));
+        System.out.println(ANSI_PURPLE + "Nombre de simbols de l'alfabet: " + ANSI_RESET + alfabetNombre);
+        System.out.println(ANSI_PURPLE + "Alfabet: " + ANSI_RESET + Arrays.toString(alfabet));
+
     }
 
-    private String[] introduccioAlfabetLletres(int alfabetNombre) {
+    /**
+     * Aquesta funcio retorna una array de strings amb l'alfabet que l'usuari vulgui
+     **/
+    private String[] introduccioAlfabet(int alfabetNombre) {
         String[] alfabet = new String[alfabetNombre];
         for (int i = 0; i < alfabetNombre; i++) {
             alfabet[i] = readLine("Introdueix el símbol " + (i + 1) + ": \n");
@@ -43,19 +47,17 @@ public class LecturaAutomat extends CommandLineProgram {
         return alfabet;
     }
 
-    private int[] introduccioAlfabetBinari(int alfabetNombre) {
-        int[] alfabet = new int[alfabetNombre];
-        for (int i = 0; i < alfabetNombre; i++) {
-            alfabet[i] = readInt("Introdueix el símbol " + (i + 1) + ": \n");
-        }
-        return alfabet;
-    }
 
-    private int[] introduccioestatsFinals(int nombreFinals) {
-        int[] finals = new int[nombreFinals];
+    /**
+     * Aquesta funcio retorna una array de strings amb els estats finals
+     **/
+    private String[] introduccioestatsFinals(int nombreFinals) {
+        String[] finals = new String[nombreFinals];
         for (int i = 0; i < nombreFinals; i++) {
-            finals[i] = readInt("Introdueix l'estat final " + (i + 1) + " \n");
+            finals[i] = readLine("Introdueix l'estat final " + (i + 1) + " \n");
         }
         return finals;
     }
+
+
 }
